b43c9cf6a381a2b81c8df6b20a61922b64aaa77b
Steffan Mouton <smouton8993@gmail.com>
2020-07-18 13:30:11 -0500

    Did first challenge

--- IfAndCodeBlocks/src/net/blacksheepworks/Main.java
+++ IfAndCodeBlocks/src/net/blacksheepworks/Main.java
@@ -3,6 +3,57 @@ package net.blacksheepworks;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score == 5000){
+            System.out.println("YOur score was " + score + ".");
+
+        }
+
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+
+        // Challenge:
+        // Print out a second score on the screen with the following:
+        // score set to 10000
+        // levelCompleted set to 8
+        // bonus set to 200
+        // But make sure the first printout above still displays as well
+
+        // === Implementation 1 ===
+
+        // Set variables to new vals, then run if check and print
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if (gameOver){
+            int finalScore = score + levelCompleted * bonus;
+            System.out.println("Your final score was " + finalScore);
+        }
+
+
+        // === Implementation 2 ===
+
+        // Reset variables to original vals
+        score = 800;
+        levelCompleted = 5;
+        bonus = 100;
+
+        if (gameOver){
+
+        }
+
     }
+
+
+
+
 }
