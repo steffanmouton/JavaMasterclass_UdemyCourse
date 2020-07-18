98d08f72778533b0ac45af042689810006988898
Steffan Mouton <smouton8993@gmail.com>
2020-07-18 13:05:23 -0500

    Variable testing. Seems the same as C# so far.

--- HelloWorld_Udemy/src/Hello.java
+++ HelloWorld_Udemy/src/Hello.java
@@ -5,11 +5,5 @@ public class Hello
     {
         System.out.println("Hello Steffan!");
 
-        int myFirstNumber = (10 + 5) + (2 * 10);
-        int mySecondNumber = 12;
-        int myThirdNumber = myFirstNumber * 2;
-        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
-        int myLastOne = 1000 - myTotal;
-        System.out.println(myLastOne);
     }
 }
