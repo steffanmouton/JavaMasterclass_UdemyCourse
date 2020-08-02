2bd3f803ac4201ce00876cb702a1812da3131f99
Steffan Mouton <smouton8993@gmail.com>
2020-08-02 12:34:24 -0500

    Created Car class. Created Getters and Setters. Print model name. I assume the tutorial is soon going to cover constructors, but as of now we are setting values in main. (yuck)

---
+++ ClassesTut/src/net/blacksheepworks/Car.java
@@ -0,0 +1,21 @@
+package net.blacksheepworks;
+
+public class Car
+{
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+
+    public String getModel()
+    {
+        return model;
+    }
+
+    public void setModel(String model)
+    {
+        this.model = model;
+    }
+}
--- ClassesTut/src/net/blacksheepworks/Main.java
+++ ClassesTut/src/net/blacksheepworks/Main.java
@@ -3,6 +3,11 @@ package net.blacksheepworks;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Car porsche = new Car();
+	    Car mini = new Car();
+
+	    porsche.setModel("Carrera");
+
+        System.out.println("Model is: " + porsche.getModel());
     }
 }
