31a3bc316fffa97a97add4fd983ee57b8ebb666e
Steffan Mouton <smouton8993@gmail.com>
2020-08-02 12:05:16 -0500

    Added ReadingUserInput project.

--- ParsingDataFromString/src/net/blacksheepworks/Main.java
+++ ParsingDataFromString/src/net/blacksheepworks/Main.java
@@ -3,6 +3,16 @@ package net.blacksheepworks;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String numberAsString = "2020";
+        System.out.println("NumberAsString" + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println(numberAsString);
+        System.out.println(number);
     }
 }
