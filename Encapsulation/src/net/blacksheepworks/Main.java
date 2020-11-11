3916ccdee9e386838bdf99aef28502e07982d0c2
Steffan Mouton <smouton8993@gmail.com>
2020-11-11 13:37:03 -0600

    Implemented a class w/o encapsulation

--- Encapsulation/src/net/blacksheepworks/Main.java
+++ Encapsulation/src/net/blacksheepworks/Main.java
@@ -2,7 +2,22 @@ package net.blacksheepworks;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        Player player = new Player();
+        player.name = "Tim";
+        player.health = 20;
+        player.weapon = "Sword";
+
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.println("Reamining health = " + player.health);
+
+        damage = 11;
+        // This is why we use encapsulation. If we can edit an object from outside,
+        // things can get real messy.
+        player.health = 200;
+        player.loseHealth(damage);
+        System.out.println("Reamining health = " + player.health);
     }
 }
