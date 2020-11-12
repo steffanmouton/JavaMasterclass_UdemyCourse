b87b6048eb85265bedaa31927c416879436ed73d
Steffan Mouton <smouton8993@gmail.com>
2020-11-11 19:44:38 -0600

    Added some classes for inheritence

--- Polymorphism/src/net/blacksheepworks/Main.java
+++ Polymorphism/src/net/blacksheepworks/Main.java
@@ -1,5 +1,44 @@
 package net.blacksheepworks;
 
+
+class Movie {
+    private String name;
+
+    public Movie(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String plot(){
+        return "No Plot Here.";
+    }
+}
+
+class Jaws extends Movie{
+    public Jaws()
+    {
+        super("Jaws");
+    }
+
+    public String plot()
+    {
+        return "A shark eats lots of people.";
+    }
+}
+
+class IndependenceDay extends Movie{
+    public IndependenceDay()
+    {
+        super("Independence Day");
+    }
+}
+
+
 public class Main {
 
     public static void main(String[] args) {
